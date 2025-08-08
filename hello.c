2d584f53e1d1be4ac76faa0bb8bbb8531a866474
tyzrex <sulavbaral58@gmail.com>
2025-08-08 21:59:06 +0545

    add addition feature in our c program

--- hello.c
+++ hello.c
@@ -4,5 +4,7 @@ int main() {
     printf("Hello, World its friday today!\n");
     // Additional code can be added here
     printf("New feature implemented!\n");
+    int a = 1, b = 2;
+    printf("Sum of a and b is: %d\n", a + b);
     return 0;
 }
